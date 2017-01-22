1a746dc1605a7a9834d72231d1e3ba5dda1a09d0
Manoj Kumar Regar <regarmanojkumar@gmail.com>
2017-01-22 23:05:27 +0530

    Adding mirror

--- install.sh
+++ install.sh
@@ -53,7 +53,7 @@ if [ ! -z "$swap" ]; then
 fi
 
 if [ !-z "$mirror" ]; then
-	echo "Adding $mirror to mirrorlist"
+	echo "Adding following mirror to mirrorlist : $mirror"
 	echo "Server = $mirror" > /etc/pacman.d/mirrorlist.insnew
 	cat /etc/pacman.d/mirrorlist >> /etc/pacman.d/mirrorlist.insnew
 	cat /etc/pacman.d/mirrorlist.insnew > /etc/pacman.d/mirrorlist
